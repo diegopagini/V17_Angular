d350ad1ecd2a5df8bb0132435bdccebbd329d473
Diego Hernan Pagini <diego.pagini@neoris.com>
2023-11-20 12:24:07 +0100

    changeDetection

--- README.md
+++ README.md
@@ -123,3 +123,55 @@ export class TitleComponent {
   </ul>
 </div>
 ```
+
+## ChangeDetection
+
+```typescript
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { TitleComponent } from "@shared/title/title.component";
+
+@Component({
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="Change Detection" />
+    <!-- Signal -->
+    <pre
+      >{{ frameworkHasSignal() | json }}
+    </pre
+    >
+    <!-- Property -->
+    <pre
+      >{{ frameworkHasProperty | json }}
+    </pre
+    >
+  `,
+})
+export default class ChangeDetectionComponent {
+  frameworkHasSignal = signal({
+    name: "Angular",
+    releaseDate: 2016,
+  });
+
+  frameworkHasProperty = {
+    name: "Angular",
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // With ChangeDetectionStrategy.OnPush the change will not be visible
+      // this.frameworkHasProperty.name = 'React';
+
+      this.frameworkHasSignal.update((framework) => ({
+        ...framework,
+        name: "React",
+      }));
+
+      console.log("Done");
+    }, 3000);
+  }
+}
+```
--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="Change Detection" />
+    <!-- Signal -->
+    <pre
+      >{{ frameworkHasSignal() | json }}
+    </pre
+    >
+    <!-- Property -->
+    <pre
+      >{{ frameworkHasProperty | json }}
+    </pre
+    >
+  `,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  frameworkHasSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  frameworkHasProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // With ChangeDetectionStrategy.OnPush the change will not be visible
+      // this.frameworkHasProperty.name = 'React';
+
+      this.frameworkHasSignal.update((framework) => ({
+        ...framework,
+        name: 'React',
+      }));
+
+      console.log('Done');
+    }, 3000);
+  }
+}
