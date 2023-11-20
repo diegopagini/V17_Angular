d7bada815abc1261073944f7c33cc204448dd8ac
Diego Hernan Pagini <diego.pagini@neoris.com>
2023-11-20 08:50:24 +0100

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dashboard-v17';
-}
+export class AppComponent {}
